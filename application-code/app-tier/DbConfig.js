3b524fb2a0bd85b022e332b3af1a2dfc12e2af91
Linekar15 <115005377+Linekar15@users.noreply.github.com>
2024-02-09 12:00:25 -0500

    Update DbConfig.js
    Configure App Instance to connect with DB

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1-instance-1.cz42s486expi.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Fsduayzr!23',
+    DB_DATABASE : 'webappdb'
 });
